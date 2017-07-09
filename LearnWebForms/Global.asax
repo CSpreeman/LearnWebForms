a7fbf8071e3a84edb02cd526d0e3b3eed8f0a9ee
Craig <craigspreeman@hotmail.com>
2017-07-09 13:33:25 -0700

    Add project files.

---
+++ LearnWebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearnWebForms.Global" Language="C#" %>
---
+++ LearnWebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnWebForms.Startup))]
+namespace LearnWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
